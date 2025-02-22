b9cdfb57b43205db1e71941b6d3fb7687452a77f
sylviaOsinaku <osinakusylvia@gmail.com>
2025-02-22 04:17:46 +0100

    added

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import vercel from 'vite-plugin-vercel';
  
-export default defineConfig({
-  server: {
-    port: process.env.PORT,
-  },
-  plugins: [vercel()],
+export default defineConfig(() => {
+  return {
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    },
+  };
 });
