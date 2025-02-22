7b1334c496b611c2eb44ed177a7ed075a08acd9c
sylviaOsinaku <osinakusylvia@gmail.com>
2025-02-22 04:15:45 +0100

    added

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import vercel from 'vite-plugin-vercel';
+ 
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT,
+  },
+  plugins: [vercel()],
+});
